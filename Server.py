3e15a889e4761bcb9579f208cf50aff7d4671e62
LZH_Win <1211574210@qq.com>
2019-12-14 12:01:16 +0800

    change ip to localhost!

--- Server.py
+++ Server.py
@@ -7,7 +7,7 @@ import logging
 
 # Tcp Server
 class Charserver(object):
-    def __init__(self, ip="192.168.2.101", port=12345):
+    def __init__(self, ip="127.0.0.1", port=12345):
         self.addr = (ip, port)
         self.sock = socket.socket()
 
